520a9c21d5d83157084f8a98569b27f1dd542a31
Ryan-Mosier <ryan.m.mosier@gmail.com>
2025-01-27 21:22:17 -0700

    changed hierarchy

--- controllers/lab1/lab1.py
+++ controllers/lab1/lab1.py
@@ -1,20 +1,18 @@
 from controller import Robot, DistanceSensor, Motor, LightSensor
 
-# time in [ms] of a simulation step
+# Simulation time step in milliseconds
 TIME_STEP = 64
-
 MAX_SPEED = 6.28
 
-# create the Robot instance.
+# Create the robot instance
 robot = Robot()
 
-# initialize devices
+# Initialize distance sensors
 ps = []
 psNames = [
     'ps0', 'ps1', 'ps2', 'ps3',
     'ps4', 'ps5', 'ps6', 'ps7'
 ]
-
 ls = []
 lsNames = [
     'ls0', 'ls1', 'ls2', 'ls3',
@@ -27,6 +25,7 @@ for i in range(8):
     ps[i].enable(TIME_STEP)
     ls[i].enable(TIME_STEP)
 
+# Initialize motors
 leftMotor = robot.getDevice('left wheel motor')
 rightMotor = robot.getDevice('right wheel motor')
 leftMotor.setPosition(float('inf'))
@@ -34,118 +33,118 @@ rightMotor.setPosition(float('inf'))
 leftMotor.setVelocity(0.0)
 rightMotor.setVelocity(0.0)
 
-global turn_status
-turn_status = False
-global wall_status
-wall_status = False
 
-
-def turn180():
-    global turn_status
-    turn_status = True
-    leftMotor.setVelocity(MAX_SPEED)
-    rightMotor.setVelocity(-MAX_SPEED)
-
-    start_time = robot.getTime() * 1000  # convert to ms
-    while (robot.getTime() * 1000 - start_time) < 4000:
-        robot.step(TIME_STEP)
-
-    # Stop the motors after the turn
-    leftMotor.setVelocity(0.0)
-    rightMotor.setVelocity(0.0)
-
-    turn_status = False
-
-
-def follow_left():
-    global turn_status
-    turn_status = True
-
-    rightMotor.setVelocity(MAX_SPEED * 0.1)
-    leftMotor.setVelocity(-MAX_SPEED * 0.1)
-
-    start_time = robot.getTime() * 1000  # convert to ms
-    while (robot.getTime() * 1000 - start_time) < 1000:
-        robot.step(TIME_STEP)
-
-    # Stop the motors after adjustment
-    leftMotor.setVelocity(0.0)
-    rightMotor.setVelocity(0.0)
-
-    turn_status = False
-
-
-def turn_right_90():
-    global turn_status
-    turn_status = True
-
-    rightMotor.setVelocity(-MAX_SPEED * 0.1)
-    leftMotor.setVelocity(MAX_SPEED * 0.1)
-
-    start_time = robot.getTime() * 1000  # convert to ms
+### used AI to help make these turning functions. Realized needed to time the turns,
+### but didnt know what the syntax was for webots/python
+### I knew i needed to turn wheels for certain amount of time just didnt know how
+def turn_right():
+    start_time = robot.getTime() * 1000  # Convert to ms
     while (robot.getTime() * 1000 - start_time) < 500:
+        leftMotor.setVelocity(0.5 * MAX_SPEED)
+        rightMotor.setVelocity(-0.5 * MAX_SPEED)
         robot.step(TIME_STEP)
-
-    # Stop the motors after adjustment
     leftMotor.setVelocity(0.0)
     rightMotor.setVelocity(0.0)
 
-    # Move forward a bit to avoid immediate re-detection
-    leftMotor.setVelocity(0.5 * MAX_SPEED)
-    rightMotor.setVelocity(0.5 * MAX_SPEED)
 
-    start_time = robot.getTime() * 1000  # convert to ms
+def turn_left():
+    start_time = robot.getTime() * 1000  # Convert to ms
     while (robot.getTime() * 1000 - start_time) < 500:
+        leftMotor.setVelocity(-0.5 * MAX_SPEED)
+        rightMotor.setVelocity(0.5 * MAX_SPEED)
         robot.step(TIME_STEP)
-
     leftMotor.setVelocity(0.0)
     rightMotor.setVelocity(0.0)
 
-    turn_status = False
+
+def turn_180():
+    print("Turning 180 degrees")
+    start_time = robot.getTime() * 1000  # Convert to ms
+    while (robot.getTime() * 1000 - start_time) < 1500:
+        leftMotor.setVelocity(-0.5 * MAX_SPEED)
+        rightMotor.setVelocity(0.5 * MAX_SPEED)
+        robot.step(TIME_STEP)
+    leftMotor.setVelocity(0.0)
+    rightMotor.setVelocity(0.0)
 
 
-# feedback loop: step simulation until receiving an exit event
-step = robot.step(TIME_STEP)
+# Global variable to track turn direction
+turnOnBlock = 0
+currentTurnDirection = 1  # 0 is for going right, 1 for left
+
+# Main loop: Continue until an exit event occurs
 while robot.step(TIME_STEP) != -1:
-    if not turn_status:
-        # read sensors outputs
-        psValues = []
-        lsValues = []
-        for i in range(8):
-            psValues.append(ps[i].getValue())
-            lsValues.append(ls[i].getValue())
+    # Get sensor readings
+    psValues = []
+    lsValues = []
+    for i in range(8):
+        psValues.append(ps[i].getValue())
+        lsValues.append(ls[i].getValue())
 
+    # Obstacle detection
+    front_obstacle = psValues[0] > 150.0 or psValues[7] > 150.0
+    right_obstacle = psValues[0] > 80.0 or psValues[1] > 80.0 or psValues[2] > 80.0
+    left_obstacle = psValues[5] > 80.0 or psValues[6] > 80.0 or psValues[7] > 80.0
+    front_light = lsValues[6] == 0.0 and lsValues[7] == 0.0 and lsValues[0] == 0.0
 
-        # detect obstacles
-        right_obstacle = psValues[1] > 100 or psValues[2] > 100
-        left_obstacle = psValues[5] > 100 or psValues[6] > 100
-        front_obstacle = psValues[0] > 150 or psValues[7] > 150
-        block_left = psValues[4] > 90 and psValues[0] < 150 and psValues[5] < 100
+    # Default motor speeds
+    leftSpeed = 0.5 * MAX_SPEED
+    rightSpeed = 0.5 * MAX_SPEED
 
-        # initialize motor speeds at 50% of MAX_SPEED.
-        leftSpeed = 0.5 * MAX_SPEED
-        rightSpeed = 0.5 * MAX_SPEED
+    # print("Light Sensor Values: ", lsValues)
 
-        # modify speeds according to obstacles
-        if front_obstacle and not wall_status:
-            print("Obstacle in Front")
-            wall_status = True
+    if turnOnBlock == 1:
+        if not front_obstacle and not right_obstacle and not left_obstacle:
+            turn_left()
+            turnOnBlock = 0
+    elif turnOnBlock == 2:
+        if not front_obstacle and not right_obstacle and not left_obstacle:
+            turn_right()
+            turnOnBlock = 0
 
-        if left_obstacle and not turn_status:
-            print("Turning Right")
-            turn_right_90()
+    if front_light == True:
+        print("Front Light Detected")
+        if currentTurnDirection == 0:
+            currentTurnDirection = 1
+            turn_180()
+        elif currentTurnDirection == 1:
+            currentTurnDirection = 0
+            turn_180()
 
-        elif right_obstacle and not turn_status:
-            leftSpeed = -0.5 * MAX_SPEED
-            rightSpeed = 0.5 * MAX_SPEED
+    # logic for right turns
+    if currentTurnDirection == 0:
 
-        if block_left and not turn_status:
-            print("Activating Follow Left (Block)")
-            follow_left()
+        # Wall-following logic
+        if front_obstacle:
+            if left_obstacle:
+                print("Turning Right")
+                turnOnBlock = 1
+                turn_right()  # Hardcoded duration
+            elif right_obstacle:
+                print("Turning Left")
+                turnOnBlock = 2
+                turn_left()  # Hardcoded duration
+        elif left_obstacle:
+            leftSpeed = 0.2 * MAX_SPEED  # Slow down left motor to turn right
+        elif not left_obstacle and right_obstacle:
+            rightSpeed = 0.2 * MAX_SPEED  # Slow down right motor to turn left
 
-        # write actuators inputs
-        leftMotor.setVelocity(leftSpeed)
-        rightMotor.setVelocity(rightSpeed)
+    if currentTurnDirection == 1:
+        # Wall-following logic
+        if front_obstacle:
+            if right_obstacle:
+                print("Turning Left")
+                turnOnBlock = 2
+                turn_left()  # Hardcoded duration
+            elif left_obstacle:
+                print("Turning Right")
+                turnOnBlock = 1
+                turn_right()  # Hardcoded duration
+        elif left_obstacle:
+            leftSpeed = 0.2 * MAX_SPEED  # Slow down left motor to turn right
+        elif not left_obstacle and right_obstacle:
+            rightSpeed = 0.2 * MAX_SPEED  # Slow down right motor to turn left
 
-### running code by the numbers:
-
+    # Set motor velocities
+    leftMotor.setVelocity(leftSpeed)
+    rightMotor.setVelocity(rightSpeed)
