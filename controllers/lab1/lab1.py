59496e93e7500ce43cf23810bd3425a3e063fc88
Ryan-Mosier <ryan.m.mosier@gmail.com>
2025-01-28 04:32:44 -0700

    got working wall following behavior!

--- controllers/lab1/lab1.py
+++ controllers/lab1/lab1.py
@@ -1,150 +1,260 @@
+import numpy as np
+import math
 from controller import Robot, DistanceSensor, Motor, LightSensor
+from enum import Enum, auto
 
 # Simulation time step in milliseconds
-TIME_STEP = 64
-MAX_SPEED = 6.28
+TIME_STEP_MS = 64
+TIME_STEP = TIME_STEP_MS / 1000
 
-# Create the robot instance
-robot = Robot()
+# General Behavoir
+WHEEL_RADIUS = 0.02
+MAX_SPEED = WHEEL_RADIUS * 6.28 # max speed in m/s
+STANDARD_SPEED = MAX_SPEED / 2
 
-# Initialize distance sensors
-ps = []
-psNames = [
-    'ps0', 'ps1', 'ps2', 'ps3',
-    'ps4', 'ps5', 'ps6', 'ps7'
-]
-ls = []
-lsNames = [
-    'ls0', 'ls1', 'ls2', 'ls3',
-    'ls4', 'ls5', 'ls6', 'ls7'
-]
+# Turning behavior
+TURN_MAX = 90
+TURN_MIN = -90
+MAX_TURN_SPEED = STANDARD_SPEED
 
-for i in range(8):
-    ps.append(robot.getDevice(psNames[i]))
-    ls.append(robot.getDevice(lsNames[i]))
-    ps[i].enable(TIME_STEP)
-    ls[i].enable(TIME_STEP)
+class RobotState(Enum):
+    LEFT_WALL = auto()
+    RIGHT_WALL = auto()
+    TURN_180 = auto()
+    STOP = auto()
 
-# Initialize motors
-leftMotor = robot.getDevice('left wheel motor')
-rightMotor = robot.getDevice('right wheel motor')
-leftMotor.setPosition(float('inf'))
-rightMotor.setPosition(float('inf'))
-leftMotor.setVelocity(0.0)
-rightMotor.setVelocity(0.0)
+class Sensor:
+    def __init__(self, name, r):
+        self.name = name
+        self.sensor = r.getDevice(name)
+        angles = {
+            "ps0": 17.234570,
+            "ps1": 45.882460,
+            "ps2": 90.000153,
+            "ps3": 151.489239,
+            "ps4": 208.784979,
+            "ps5": 270.000362,
+            "ps6": 314.209213,
+            "ps7": 342.857103
+        }
+        self.angle = angles.get(name, -1)
+        table = self.sensor.getLookupTable()
+        self.lookup_table = [
+            table[i:i + 3] for i in range(0, len(table), 3)
+        ]
+        self.distances = [row[0] for row in self.lookup_table]  # Input distances
+        self.responses = [row[1] for row in self.lookup_table]  # Response values
+        self.sensor.enable(TIME_STEP_MS)
+
+    def calculate_distance(self, sensor_value):
+        if sensor_value <= self.responses[-1]:
+            return self.distances[-1]
+        elif sensor_value >= self.responses[0]:
+            return self.distances[0]
+
+        # Linear interpolation between lookup table points
+        for i in range(len(self.responses) - 1):
+            if self.responses[i] >= sensor_value >= self.responses[i + 1]:
+                # Interpolate
+                x0, y0 = self.responses[i], self.distances[i]
+                x1, y1 = self.responses[i + 1], self.distances[i + 1]
+                return y0 + (sensor_value - x0) * (y1 - y0) / (x1 - x0)
+        # Return None if no match (shouldn't happen)
+        return None
+
+    def read(self):
+        return self.calculate_distance(self.sensor.getValue())
+
+class WallFollower:
+    def __init__(self, r):
+        self.prev_distances = []
+        self.DIST_SIZE = 5
+        self.DESIRED_DIST = 3
+        self.WALL_FOLLOW_KP = 0.2
+        self.WALL_FOLLOW_KI = 0
+        self.WALL_FOLLOW_KD = 0.1
+        self.integral = 0.0
+        self.prev_error = -np.inf
+        self.robot = r
+
+    def clear(self):
+        self.prev_distances = []
+        self.integral = 0.0
+        self.prev_error = -np.inf
+
+    def execute(self):
+        dist: float = -1
+        if self.robot.state == RobotState.LEFT_WALL:
+            dist = self.robot.get_distance('left')
+
+        elif self.robot.state == RobotState.RIGHT_WALL:
+            dist = self.robot.get_distance('right')
+
+        if dist == -1:
+            raise ValueError("Distance sensor read error.")
+        dist = 100 * dist # convert from m to mm
+        if dist >= 6.8: # can't see wall
+            self.clear()
+            covered_distance = 0.0
+            while covered_distance < 0.04:
+                self.robot.go_forward(STANDARD_SPEED)
+                covered_distance += STANDARD_SPEED * TIME_STEP
+                self.robot.robot.step(TIME_STEP_MS)
+
+            if self.robot.state == RobotState.LEFT_WALL:
+                angle = self.robot.angle - 90
+                while self.robot.angle > angle:
+                    self.robot.turn(0, -90)
+                    self.robot.robot.step(TIME_STEP_MS)
+            elif self.robot.state == RobotState.RIGHT_WALL:
+                angle = self.robot.angle + 90
+                while self.robot.angle < angle:
+                    self.robot.turn(0, 90)
+                    self.robot.step(TIME_STEP_MS)
+            covered_distance = 0.0
+            while covered_distance < 0.06:
+                self.robot.go_forward(STANDARD_SPEED)
+                covered_distance += STANDARD_SPEED * TIME_STEP
+                self.robot.robot.step(TIME_STEP_MS)
+            return
+        self.prev_distances.append(dist)
+        if len(self.prev_distances) > self.DIST_SIZE:
+            self.prev_distances.pop(0)
+        avg_dist = np.mean(self.prev_distances)
+        error = self.DESIRED_DIST - avg_dist
+
+        # PID
+        self.integral += error * TIME_STEP
+        if self.prev_error != -np.inf:
+            derivative = (error - self.prev_error) / TIME_STEP
+        else:
+            derivative = 0
+        self.prev_error = error
+
+        w = (self.WALL_FOLLOW_KP * error) + (self.WALL_FOLLOW_KI * self.integral) + (
+                self.WALL_FOLLOW_KD * derivative)
+        w_deg = np.degrees(w)
+        self.robot.turn(MAX_TURN_SPEED, w_deg)
+
+class RobotClass:
+    def __init__(self):
+        self.state = RobotState.LEFT_WALL
+        self.robot = Robot()
+        self.ps:dict[str,Sensor]= {}
+        ps_names = ['ps0', 'ps1', 'ps2', 'ps3',
+                        'ps4', 'ps5', 'ps6', 'ps7']
+        self.ls:dict[str,Sensor] = {}
+        ls_names = ['ls0', 'ls1', 'ls2', 'ls3',
+                        'ls4', 'ls5', 'ls6', 'ls7']
+        for name in ps_names:
+            self.ps[name] = Sensor(name, self.robot)
+
+        for name in ls_names:
+            self.ls[name] = Sensor(name, self.robot)
+
+        self.motors = {'left': self.robot.getDevice('left wheel motor'),
+                       'right': self.robot.getDevice('right wheel motor')}
+        self.motors['left'].setPosition(float('inf'))
+        self.motors['right'].setPosition(float('inf'))
+        self.motors['left'].setVelocity(0.0)
+        self.motors['right'].setVelocity(0.0)
+        self.angle = 0
+        self.wall_follower = WallFollower(self)
+
+    def turn(self, v, w):
+        # turns the robot at w (deg/sec). + for right, - for left
+        # while maintain a forward velocity of v (m/s)
+        w = np.clip(w, TURN_MIN, TURN_MAX)
+        self.angle += (w * TIME_STEP)
+        # convert to radians
+        w_rad = np.radians(w)
+        wheelbase = 0.052  # from the e-puck proto file
+
+        v_rel = w_rad * (wheelbase / 2) # relative velocity
+
+        v_left = v + v_rel
+        v_right = v - v_rel# Adjust speeds to ensure they don't exceed max wheel speed
+        while abs(v_left) > MAX_SPEED or abs(v_right) > MAX_SPEED:
+            # Scale down v to ensure both wheel speeds stay within limits
+            scaling_factor = max(abs(v_left) / MAX_SPEED, abs(v_right) / MAX_SPEED)
+            v /= scaling_factor
+            # Recalculate v_left and v_right with the scaled v
+            v_left = v + v_rel
+            v_right = v - v_rel
 
 
-### used AI to help make these turning functions. Realized needed to time the turns,
-### but didnt know what the syntax was for webots/python
-### I knew i needed to turn wheels for certain amount of time just didnt know how
-def turn_right():
-    start_time = robot.getTime() * 1000  # Convert to ms
-    while (robot.getTime() * 1000 - start_time) < 500:
-        leftMotor.setVelocity(0.5 * MAX_SPEED)
-        rightMotor.setVelocity(-0.5 * MAX_SPEED)
-        robot.step(TIME_STEP)
-    leftMotor.setVelocity(0.0)
-    rightMotor.setVelocity(0.0)
+        #convert to w
+        w_left = v_left / WHEEL_RADIUS
+        w_right = v_right / WHEEL_RADIUS
+
+        self.motors['left'].setVelocity(w_left)
+        self.motors['right'].setVelocity(w_right)
+
+    def go_forward(self, v):
+        w = v / WHEEL_RADIUS
+        self.motors['left'].setVelocity(w)
+        self.motors['right'].setVelocity(w)
+
+    def get_distance(self, direction):
+        if direction == 'forward':
+            name:str
+            if self.state == RobotState.LEFT_WALL:
+                name = 'ps0'
+            elif self.state == RobotState.RIGHT_WALL:
+                name = 'ps7'
+            else: return -1
+            h = self.ps[name].read()
+            angle = self.ps[name].angle
+            return h * math.cos(np.radians(angle))
 
 
-def turn_left():
-    start_time = robot.getTime() * 1000  # Convert to ms
-    while (robot.getTime() * 1000 - start_time) < 500:
-        leftMotor.setVelocity(-0.5 * MAX_SPEED)
-        rightMotor.setVelocity(0.5 * MAX_SPEED)
-        robot.step(TIME_STEP)
-    leftMotor.setVelocity(0.0)
-    rightMotor.setVelocity(0.0)
+        elif direction == 'backward':
+            name: str
+            if self.state == RobotState.LEFT_WALL:
+                name = 'ps3'
+            elif self.state == RobotState.RIGHT_WALL:
+                name = 'ps4'
+            else:
+                return -1
+            h = self.ps[name].read()
+            angle = self.ps[name].angle
+            return h * math.cos(np.radians(angle))
+        elif direction == 'left':
+            return self.ps['ps5'].read()
+        elif direction == 'right':
+            return self.ps['ps2'].read()
 
+    # def evaluate_state(self):
+        # TODO
 
-def turn_180():
-    print("Turning 180 degrees")
-    start_time = robot.getTime() * 1000  # Convert to ms
-    while (robot.getTime() * 1000 - start_time) < 1500:
-        leftMotor.setVelocity(-0.5 * MAX_SPEED)
-        rightMotor.setVelocity(0.5 * MAX_SPEED)
-        robot.step(TIME_STEP)
-    leftMotor.setVelocity(0.0)
-    rightMotor.setVelocity(0.0)
+    def execute_state(self):
+        # TODO
 
+        if self.state == RobotState.STOP:
+            return  #do nothing
+        if self.state == RobotState.LEFT_WALL or self.state == RobotState.RIGHT_WALL:
+            forward_dist = self.get_distance('forward')
+            forward_dist *= 100
+            if forward_dist > self.wall_follower.DESIRED_DIST:
+                self.wall_follower.execute()
+            else:
+                self.wall_follower.clear()
+                if self.state == RobotState.LEFT_WALL:
+                    angle = self.angle + 90
+                    while self.angle < angle:
+                        self.turn(0, 90)
+                        self.robot.step(TIME_STEP_MS)
+                elif self.state == RobotState.RIGHT_WALL:
+                    angle = self.angle - 90
+                    while self.angle > angle:
+                        self.turn(0, -90)
+                        self.robot.step(TIME_STEP_MS)
 
 # Global variable to track turn direction
-turnOnBlock = 0
-currentTurnDirection = 1  # 0 is for going right, 1 for left
+robot = RobotClass()
 
 # Main loop: Continue until an exit event occurs
-while robot.step(TIME_STEP) != -1:
-    # Get sensor readings
-    psValues = []
-    lsValues = []
-    for i in range(8):
-        psValues.append(ps[i].getValue())
-        lsValues.append(ls[i].getValue())
-
-    # Obstacle detection
-    front_obstacle = psValues[0] > 150.0 or psValues[7] > 150.0
-    right_obstacle = psValues[0] > 80.0 or psValues[1] > 80.0 or psValues[2] > 80.0
-    left_obstacle = psValues[5] > 80.0 or psValues[6] > 80.0 or psValues[7] > 80.0
-    front_light = lsValues[6] == 0.0 and lsValues[7] == 0.0 and lsValues[0] == 0.0
-
-    # Default motor speeds
-    leftSpeed = 0.5 * MAX_SPEED
-    rightSpeed = 0.5 * MAX_SPEED
-
-    # print("Light Sensor Values: ", lsValues)
-
-    if turnOnBlock == 1:
-        if not front_obstacle and not right_obstacle and not left_obstacle:
-            turn_left()
-            turnOnBlock = 0
-    elif turnOnBlock == 2:
-        if not front_obstacle and not right_obstacle and not left_obstacle:
-            turn_right()
-            turnOnBlock = 0
-
-    if front_light == True:
-        print("Front Light Detected")
-        if currentTurnDirection == 0:
-            currentTurnDirection = 1
-            turn_180()
-        elif currentTurnDirection == 1:
-            currentTurnDirection = 0
-            turn_180()
-
-    # logic for right turns
-    if currentTurnDirection == 0:
-
-        # Wall-following logic
-        if front_obstacle:
-            if left_obstacle:
-                print("Turning Right")
-                turnOnBlock = 1
-                turn_right()  # Hardcoded duration
-            elif right_obstacle:
-                print("Turning Left")
-                turnOnBlock = 2
-                turn_left()  # Hardcoded duration
-        elif left_obstacle:
-            leftSpeed = 0.2 * MAX_SPEED  # Slow down left motor to turn right
-        elif not left_obstacle and right_obstacle:
-            rightSpeed = 0.2 * MAX_SPEED  # Slow down right motor to turn left
-
-    if currentTurnDirection == 1:
-        # Wall-following logic
-        if front_obstacle:
-            if right_obstacle:
-                print("Turning Left")
-                turnOnBlock = 2
-                turn_left()  # Hardcoded duration
-            elif left_obstacle:
-                print("Turning Right")
-                turnOnBlock = 1
-                turn_right()  # Hardcoded duration
-        elif left_obstacle:
-            leftSpeed = 0.2 * MAX_SPEED  # Slow down left motor to turn right
-        elif not left_obstacle and right_obstacle:
-            rightSpeed = 0.2 * MAX_SPEED  # Slow down right motor to turn left
-
+while robot.robot.step(TIME_STEP_MS) != -1:
     # Set motor velocities
-    leftMotor.setVelocity(leftSpeed)
-    rightMotor.setVelocity(rightSpeed)
+    # robot.evaluate_state()
+    robot.execute_state()
